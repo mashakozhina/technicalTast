acd1d726e86c5ca3c92cbc586bbeeecbbec2c6d7
“mashakozhina” <masha.kozhina@rakuten.com>
2021-11-29 15:57:54 +0100

    FictCommit

---
+++ integration/user/login.js
@@ -0,0 +1,18 @@
+///<reference types='Cypress' />
+
+describe('User login', () => {
+
+  beforeEach(() => {
+      
+  });
+
+  it('Login as existed user', () => {
+  
+  });
+
+  it('Validation for not existede user', () => {
+  
+});
+
+  
+});
